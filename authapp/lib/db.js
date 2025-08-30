51baa68363b50dad62ddec036c2553b60364be56
editid0 <66117959+editid0@users.noreply.github.com>
2025-08-30 19:46:04 +0100

    get rid of sessions table

--- authapp/lib/db.js
+++ authapp/lib/db.js
@@ -1,11 +1,3 @@
-// CREATE TABLE sessions (
-//   id SERIAL PRIMARY KEY,
-//   user_id INT REFERENCES users(id) ON DELETE CASCADE,
-//   token TEXT UNIQUE NOT NULL,
-//   expires_at TIMESTAMP NOT NULL,
-//   created_at TIMESTAMP DEFAULT NOW()
-// );
-
 // CREATE TABLE users (
 //   id SERIAL PRIMARY KEY,
 //   username TEXT UNIQUE NOT NULL,
